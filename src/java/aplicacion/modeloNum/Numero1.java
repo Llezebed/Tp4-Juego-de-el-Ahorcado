466c0aef54175706bc94950430d13e52fa0ebe96
sandra <sandra@LAPTOP-A1G2QE10>
2018-05-20 18:34:56 -0300

    Se agregó la clase Numero Por:Ponce Luana

---
+++ src/java/aplicacion/modeloNum/Numero1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.modeloNum;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author sandra
+ */
+public class Numero1 implements Serializable {
+ private int n;
+
+    /**
+     * @return the n
+     */
+    public int getN() {
+        return n;
+    }
+
+    /**
+     * @param n the n to set
+     */
+    public void setN(int n) {
+        this.n = n;
+    }
+ 
+}
